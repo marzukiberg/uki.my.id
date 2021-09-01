40b56c6637597674a231768ed5d0858e34aab476
marzukiberg <marzukiberg@gmail.com>
2021-09-01 17:36:37 +0700

    added screenshot

--- src/components/organisms/Hero/BackgroundImages.jsx
+++ src/components/organisms/Hero/BackgroundImages.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 const BackgroundImages = () => {
   return (
     <>
-      <img src="/img/logos.png" alt="Logos" className="" data-aos="fade-in" />
-      <div data-aos="fade-in">
-        <img
-          src="/img/profile.jpeg"
-          alt="Profile Picture"
-          className="w-[300px] absolute left-1/2 bottom-0 transform translate-x-[-50%]"
-        />
-      </div>
+      <img src="/img/logos.png" alt="Logos" className="" />
+
+      <img
+        src="/img/profile.jpeg"
+        alt="Profile Picture"
+        className="w-[300px] absolute left-1/2 bottom-0 transform translate-x-[-50%]"
+      />
     </>
   );
 };
