86207f7cabc6dc4a79c2166760f91d95d71ddfd6
marzukiberg <marzukiberg@gmail.com>
2022-01-16 07:58:21 +0700

    loader url changed

--- utils/img-loader.js
+++ utils/img-loader.js
@@ -1,5 +1,5 @@
 export const imgLoader = ({ src, width, quality }) => {
-  return `http://localhost:3000${src}?w=${width}&q=${quality || 75}`;
+  return `https://uki.vercel.app${src}?w=${width}&q=${quality || 75}`;
 };
 
 export const externalImgLoader = ({ src, width, quality }) => {
